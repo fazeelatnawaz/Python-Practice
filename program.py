e3abf761fe3ba92d392efab4bbed8a97e4bcb827
fazeelatnawaz <nawazfazeelat0@gmail.com>
2025-01-30 19:57:51 +0500

    my python practice code

---
+++ program.py
@@ -0,0 +1,32 @@
+def subtract(a,b):
+    return a-b
+print(subtract(15,10))
+    
+def hello_world ():
+    return print("hello world")
+hello_world()
+
+
+
+def division(x,y):
+    return x/y
+print(division(y=30,x=5))
+
+def division(x,y):
+    return x/y
+print(division(y=5,x=30))
+
+
+
+a = 10, b = 20, c = 30
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
+
+    
+
+
+
---
+++ test.py
@@ -0,0 +1,32 @@
+a = 10
+b = 20
+c = 30
+
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
+
+
+    a = 30
+    b = 60
+    c = 90
+
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
+
+    a=20
+    b=5
+    c=3
+    if a>b and a>c:
+       print(a)
+    elif b>a and b>c:
+        print(b)
+    else:
+        print(c)
